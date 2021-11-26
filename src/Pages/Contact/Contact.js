e52f1ad43abc251dc4a05a80e37e7561f8de9dd3
srrobin <srrobin07@gmail.com>
2021-11-26 07:14:10 +0600

    update

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -30,7 +30,7 @@ const Contact = () => {
           
             <div className="contact-form"> 
              <h4 className="font-weight-light">Contact With Me </h4> 
-                <form name="contact"  netlify> 
+                <form name="contact" method="post" data-netlify="true"> 
                     <p> <label for="name">Name</label><input type="text" name="name" placeholder="write your name" /></p>
                     <p> <label for="email">Email</label><input type="email" name="email" placeholder="write your email number" /></p>
                     <p> <label for="tel">Phone Number</label><input type="tel" name="tel" placeholder="write your phone number" /></p>
