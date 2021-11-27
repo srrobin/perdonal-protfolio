92a9304f779ad570c0f3eae272eacf7b9deff061
srrobin <srrobin07@gmail.com>
2021-11-27 11:28:06 +0600

    e

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -30,7 +30,9 @@ const Contact = () => {
           
             <div className="contact-form"> 
              <h4 className="font-weight-light">Contact With Me </h4> 
-                <form name="contact"  method="post" netlify netlify-honeypot="bot-field" > 
+             <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                {/* You still need to add the hidden input with the form name to your JSX form */}    
+                <input type="hidden" name="form-name" value="contact" />
                     <p> <label for="name">Name</label><input type="text" name="name" placeholder="write your name" /></p>
                     <p> <label for="email">Email</label><input type="email" name="email" placeholder="write your email number" /></p>
                     <p> <label for="tel">Phone Number</label><input type="tel" name="tel" placeholder="write your phone number" /></p>
